7180f593ad3f5104f14d0693abc2658e958b55a9
tim chen <chentim594@gmail.com>
2025-12-24 12:12:00 +0800

    Initial Commit

---
+++ EarthLord day7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EarthLord day7
+//
+//  Created by 996 on 2025/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EarthLord day7/EarthLord_day7App.swift
@@ -0,0 +1,17 @@
+//
+//  EarthLord_day7App.swift
+//  EarthLord day7
+//
+//  Created by 996 on 2025/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EarthLord_day7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
